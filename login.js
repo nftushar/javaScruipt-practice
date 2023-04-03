7b7e97d597e4fe0eff166bd8e6b3c2d832c8bf63
nftushar <nftushar5200@gmail.com>
2023-04-03 20:45:54 +0600

    javascript Practice

---
+++ login.js
@@ -0,0 +1,31 @@
+// document.addEventListener('DOMContentLoaded', function(){  })
+// document.getElementById("form").addEventListener("submit", function (event) {
+//     event.preventDefault()
+//     // console.log("hello");
+//     let emailFild = document.getElementById('user-email');
+//     // const email = emailFild.value;
+//     console.log(emailFild.value);
+
+//     let passwordFild = document.getElementById('user-password');
+//     // let password = passwordFild.value;
+//     console.log(passwordFild.value);
+
+// });
+
+
+document.getElementById('btn-submit').addEventListener('click', function () {
+
+    // console.log("hello");
+    let emailFild = document.getElementById('user-email');
+    // const email = emailFild.value;
+    console.log(emailFild.value);
+
+    let passwordFild = document.getElementById('user-password');
+    // let password = passwordFild.value;
+    console.log(passwordFild.value);
+
+
+})
+
+
+
